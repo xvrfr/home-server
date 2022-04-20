9062b39e6aecf66a020df81e363b8dd5a8b232ed
xvrfr <xvr@bk.ru>
2022-04-20 18:09:54 +0300

    Update virtualbox_headless_installer.sh

--- virtualbox-headless/files/virtualbox_headless_installer.sh
+++ virtualbox-headless/files/virtualbox_headless_installer.sh
@@ -1,1 +1,16 @@
+#!/usr/bin/env bash
+set -e
 
+declare -a MISSING_PACKAGES
+
+function info { echo -e "\e[32m[info] $*\e[39m"; }
+function warn  { echo -e "\e[33m[warn] $*\e[39m"; }
+function error { echo -e "\e[31m[error] $*\e[39m"; exit 1; }
+
+command -v dpkg-de1b > /dev/null 2>&1 || MISSING_PACKAGES+=("dpkg-deb")
+
+if [ ! -z "${MISSING_PACKAGES}" ]; then
+    error "Missing: ${MISSING_PACKAGES[@]}"
+    warn "Are you sure you have DEBIAN-like system?"
+    warn "Please, install and configure missing packages before running this script again."
+fi
