f40ea5742a8301a7c3f8b402dfb82c62621b74a7
xvrfr <xvr@bk.ru>
2022-04-22 15:13:50 +0300

    Update x11vnc_configurator.sh

--- x11vnc-setup/files/x11vnc_configurator.sh
+++ x11vnc-setup/files/x11vnc_configurator.sh
@@ -11,12 +11,27 @@ X11VNC_PASSWD_FILE="/etc/x11vnc.passwd"
 X11VNC_PASSWD=x11vnc
 
 # Creating password file
-sudo x11vnc -storepasswd $X11VNC_PASSWD $X11VNC_PASSWD_FILE ;
+x11vnc -storepasswd $X11VNC_PASSWD $X11VNC_PASSWD_FILE ;
 chmod 0777 $X11VNC_PASSWD_FILE
 
+# Creating service definition file for autostart on reboot
+cat > /lib/systemd/system/x11vnc.service << EOF
+[Unit]
+Description=Start x11vnc
+After=multi-user.target
 
+[Service]
+Type=simple
+ExecStart=/usr/bin/x11vnc -display :0 -auth guess -forever -loop -noxdamage -repeat -localhost -rfbauth /etc/x11vnc.passwd -rfbport 5900 -shared
 
+[Install]
+WantedBy=multi-user.target
+EOF
 
+# Starting service
+systemctl enable x11vnc.service
+systemctl start x11vnc.service
+systemctl daemon-reload
 
 # Cleanup
 X11VNC_PASSWD=
