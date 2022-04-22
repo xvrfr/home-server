28ba5a5d68ca60b241aa4f486ae354c2cf763de4
xvrfr <xvr@bk.ru>
2022-04-22 13:30:05 +0300

    Update x11vnc_configurator.sh

--- x11vnc-setup/files/x11vnc_configurator.sh
+++ x11vnc-setup/files/x11vnc_configurator.sh
@@ -6,12 +6,15 @@ set -e
 # https://docs.j7k6.org/x11vnc-debian-ubuntu-linux/
 # https://help.ubuntu.com/community/VNC/Servers
 
+echo X11VNC_PASSWD=$X11VNC_PASSWD
 
 X11VNC_PASSWD_DEFAULT="/etc/x11vnc.passwd"
+X11VNC_PASSWD=
+
 
 set_passwd ()
 {
-    X11VNC_PASSWD=$(sudo x11vnc -storepasswd $X11VNC_PASSWD_DEFAULT | awk -F': ' 'END{print$2}') ;
+    X11VNC_PASSWD=$(sudo x11vnc -storepasswd /etc/x11vnc.passwd | awk -F': ' 'END{print$2}') ;
 }
 
 set_passwd ;
