08a63d44087df9e99d793ced2fa5421bd1a6aa0f
xvrfr <xvr@bk.ru>
2022-04-23 00:07:06 +0300

    Create x11vnc_reconfigurator_from_any_ip_access.sh

---
+++ x11vnc-setup/files/x11vnc_reconfigurator_from_any_ip_access.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+
+# Set defaults
+X11VNC_USER=x11vnc
+
+id $X11VNC_USER || adduser $X11VNC_USER -s /sbin/nologin 
+
+# Creating service definition file for autostart on reboot
+sudo cat > /lib/systemd/system/x11vnc.service << EOF
+[Unit]
+Description=Start x11vnc
+After=multi-user.target
+
+[Service]
+User=$X11VNC_USER
+Type=simple
+ExecStart=/usr/bin/x11vnc -display :0 -auth guess -forever -loop -noxdamage -repeat -rfbauth /etc/x11vnc.passwd -rfbport 5900 -shared
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Re-starting service
+systemctl stop x11vnc.service
+systemctl start x11vnc.service
+systemctl daemon-reload
+
