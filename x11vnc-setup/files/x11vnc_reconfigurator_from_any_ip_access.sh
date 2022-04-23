764f3ec7c54ab7fbaeb9d3e0ff997f93778a12ca
xvrfr <xvr@bk.ru>
2022-04-23 17:25:11 +0300

    Update x11vnc_reconfigurator_from_any_ip_access.sh

--- x11vnc-setup/files/x11vnc_reconfigurator_from_any_ip_access.sh
+++ x11vnc-setup/files/x11vnc_reconfigurator_from_any_ip_access.sh
@@ -21,7 +21,7 @@ After=multi-user.target
 [Service]
 #User=$X11VNC_USER
 Type=simple
-ExecStart=/usr/bin/x11vnc -display :0 -auth $X11VNC_XAUTH -forever -loop -noxdamage -repeat -rfbauth $X11VNC_PASSWD_FILE -rfbport 5900 -shared
+ExecStart=/usr/bin/x11vnc -display :0 -auth "$X11VNC_XAUTH" -forever -loop -noxdamage -repeat -rfbauth $X11VNC_PASSWD_FILE -rfbport 5900 -shared
 
 [Install]
 WantedBy=multi-user.target
