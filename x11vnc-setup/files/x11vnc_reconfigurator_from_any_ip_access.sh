6999141988d6823da66ba1872e2c47088db2e3c6
xvrfr <xvr@bk.ru>
2022-04-23 15:57:42 +0300

    Update x11vnc_reconfigurator_from_any_ip_access.sh

--- x11vnc-setup/files/x11vnc_reconfigurator_from_any_ip_access.sh
+++ x11vnc-setup/files/x11vnc_reconfigurator_from_any_ip_access.sh
@@ -1,34 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-
-xauth list 
-
-# Rename the existing .Xauthority file by running the following command
-mv ~/.Xauthority ~/old.Xauthority 
-
-# xauth with complain unless ~/.Xauthority exists
-touch ~/.Xauthority
-
-# only this one key is needed for X11 over SSH 
-echo xauth gen
-xauth generate :0 . trusted 
-
-# generate our own key, xauth requires 128 bit hex encoding
-echo xauth add
-xauth add ${HOST}:0 . $(xxd -l 16 -p /dev/urandom)
-
-# To view a listing of the .Xauthority file, enter the following 
-echo xauth list
-xauth list 
-
-exit
-
-
 # Set defaults
-X11VNC_USER=x11vnc
+X11VNC_XAUTH="$(ls /var/run/xauth/{*} || ls /var/run/sddm/{*} || echo guess )"
+#X11VNC_USER=x11vnc
 
-id $X11VNC_USER || ( useradd $X11VNC_USER && ( echo "$X11VNC_USER:$X11VNC_USER" | chpasswd ))
+#id $X11VNC_USER || ( useradd $X11VNC_USER && ( echo "$X11VNC_USER:$X11VNC_USER" | chpasswd ))
 
 # Creating service definition file for autostart on reboot
 sudo cat > /lib/systemd/system/x11vnc.service << EOF
@@ -37,9 +14,9 @@ Description=Start x11vnc
 After=multi-user.target
 
 [Service]
-User=$X11VNC_USER
+#User=$X11VNC_USER
 Type=simple
-ExecStart=/usr/bin/x11vnc -display :0 -auth guess -forever -loop -noxdamage -repeat -rfbauth /etc/x11vnc.passwd -rfbport 5900 -shared
+ExecStart=/usr/bin/x11vnc -display :0 -auth $X11VNC_XAUTH -forever -loop -noxdamage -repeat -rfbauth /etc/x11vnc.passwd -rfbport 5900 -shared
 
 [Install]
 WantedBy=multi-user.target
