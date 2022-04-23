cc1c0d15723566b98c9fbb65d1bd3e68957206c3
xvrfr <xvr@bk.ru>
2022-04-24 00:37:23 +0300

    Update x11vnc_configurator.sh

--- x11vnc-setup/files/x11vnc_configurator.sh
+++ x11vnc-setup/files/x11vnc_configurator.sh
@@ -5,9 +5,19 @@ function info { echo -e "\e[32m[info] $*\e[39m"; }
 function warn  { echo -e "\e[33m[warn] $*\e[39m"; }
 
 # Set defaults
-X11VNC_XAUTH="$(ls /var/run/xauth/{*} 2>/dev/null || ls /var/run/sddm/{*} 2>/dev/null || echo guess )"
 X11VNC_PASSWD_FILE="/etc/x11vnc.passwd"
 X11VNC_PASSWD=x11vnc
+
+X11VNC_XAUTH='$('
+X11VNC_XAUTH+="xauth info 2>/dev/null | tac | xargs | grep -Po '^([^:]*:){5}[[:blank:]]+[^0]([^:]*:[[:blank:]])*\K.*'"
+X11VNC_XAUTH+=' || '
+X11VNC_XAUTH+='ls /var/run/xauth/{*} 2>/dev/null'
+X11VNC_XAUTH+=' || '
+X11VNC_XAUTH+='ls /var/run/sddm/{*} 2>/dev/null'
+X11VNC_XAUTH+=' || '
+X11VNC_XAUTH+='echo guess'
+X11VNC_XAUTH+=' )'
+
 
 info "x11vnc will go with -auth ""$X11VNC_XAUTH"""
 
@@ -29,7 +39,8 @@ After=multi-user.target
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/x11vnc -display :0 -auth "$(ls /var/run/xauth/{*} 2>/dev/null || ls /var/run/sddm/{*} 2>/dev/null || echo guess )" -forever -loop -noxdamage -repeat -localhost -rfbauth $X11VNC_PASSWD_FILE -rfbport 5900 -shared
+ExecStartPre=/bin/sh -c "/bin/systemctl set-environment X11VNC_XAUTH=$X11VNC_XAUTH"
+ExecStart=/usr/bin/x11vnc -display :0 -auth \$X11VNC_XAUTH -forever -loop -noxdamage -repeat -localhost -rfbauth $X11VNC_PASSWD_FILE -rfbport 5900 -shared
 
 [Install]
 WantedBy=multi-user.target
