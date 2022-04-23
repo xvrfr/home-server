9430f6156df173497496f145effbf054a50a7d3f
xvrfr <xvr@bk.ru>
2022-04-23 13:57:15 +0300

    Update x11vnc_reconfigurator_from_any_ip_access.sh

--- x11vnc-setup/files/x11vnc_reconfigurator_from_any_ip_access.sh
+++ x11vnc-setup/files/x11vnc_reconfigurator_from_any_ip_access.sh
@@ -2,6 +2,26 @@
 set -e
 
 
+xauth list 
+
+# Rename the existing .Xauthority file by running the following command
+mv .Xauthority old.Xauthority 
+
+# xauth with complain unless ~/.Xauthority exists
+touch ~/.Xauthority
+
+# only this one key is needed for X11 over SSH 
+xauth generate :0 . trusted 
+
+# generate our own key, xauth requires 128 bit hex encoding
+xauth add ${HOST}:0 . $(xxd -l 16 -p /dev/urandom)
+
+# To view a listing of the .Xauthority file, enter the following 
+xauth list 
+
+exit
+
+
 # Set defaults
 X11VNC_USER=x11vnc
 
