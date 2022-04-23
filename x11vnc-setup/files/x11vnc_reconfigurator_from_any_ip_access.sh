a7f72134019a1010a9efe57153ee6a4ff37c88a9
xvrfr <xvr@bk.ru>
2022-04-23 16:10:02 +0300

    Update x11vnc_reconfigurator_from_any_ip_access.sh

--- x11vnc-setup/files/x11vnc_reconfigurator_from_any_ip_access.sh
+++ x11vnc-setup/files/x11vnc_reconfigurator_from_any_ip_access.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+function info { echo -e "\e[32m[info] $*\e[39m"; }
+
 # Set defaults
-X11VNC_XAUTH="$(ls /var/run/xauth/{*} || ls /var/run/sddm/{*} || echo guess )"
+X11VNC_XAUTH="$(ls /var/run/xauth/{*} 2>/dev/null || ls /var/run/sddm/{*} 2>/dev/null || echo guess )"
 #X11VNC_USER=x11vnc
+
+info "x11vnc will go with -auth ""$X11VNC_XAUTH"""
 
 #id $X11VNC_USER || ( useradd $X11VNC_USER && ( echo "$X11VNC_USER:$X11VNC_USER" | chpasswd ))
 
@@ -23,6 +27,13 @@ WantedBy=multi-user.target
 EOF
 
 # Re-starting service
+info "Restarting service"
 systemctl daemon-reload
 systemctl stop x11vnc.service
 systemctl start x11vnc.service
+
+info "Script completed."
+exit 0
+
+
+
