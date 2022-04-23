d7d9a7717fbd874c587165233c021f4c84f4dc78
xvrfr <xvr@bk.ru>
2022-04-23 16:27:51 +0300

    Update x11vnc_configurator.sh

--- x11vnc-setup/files/x11vnc_configurator.sh
+++ x11vnc-setup/files/x11vnc_configurator.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-# https://itsnotepad.ru/2020/11/13/x11vnc-avtozapusk-pri-starte-sistemy.html
-# https://avg-it.ru/info/articles/nastroyka-udalyennogo-dostupa-do-kompyutera-s-linux-ubuntu-debian-mint-xubuntu-elementary-os/
-# https://docs.j7k6.org/x11vnc-debian-ubuntu-linux/
-# https://help.ubuntu.com/community/VNC/Servers
+function info { echo -e "\e[32m[info] $*\e[39m"; }
+function warn  { echo -e "\e[33m[warn] $*\e[39m"; }
 
 # Set defaults
+X11VNC_XAUTH="$(ls /var/run/xauth/{*} 2>/dev/null || ls /var/run/sddm/{*} 2>/dev/null || echo guess )"
 X11VNC_PASSWD_FILE="/etc/x11vnc.passwd"
 X11VNC_PASSWD=x11vnc
+
+info "x11vnc will go with -auth ""$X11VNC_XAUTH"""
 
 # Creating password file
 x11vnc -storepasswd $X11VNC_PASSWD $X11VNC_PASSWD_FILE ;
 chmod 0777 $X11VNC_PASSWD_FILE
+
+warn "Using default password! To change password use ""x11vnc -storepasswd $X11VNC_PASSWD_FILE"" command"
+info "Created password file: ""$X11VNC_PASSWD_FILE"""
 
 # Creating service definition file for autostart on reboot
 cat > /lib/systemd/system/x11vnc.service << EOF
@@ -22,12 +26,13 @@ After=multi-user.target
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/x11vnc -display :0 -auth guess -forever -loop -noxdamage -repeat -localhost -rfbauth /etc/x11vnc.passwd -rfbport 5900 -shared
+ExecStart=/usr/bin/x11vnc -display :0 -auth $X11VNC_XAUTH -forever -loop -noxdamage -repeat -localhost -rfbauth /etc/x11vnc.passwd -rfbport 5900 -shared
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
+info "Starting service"
 # Starting service
 systemctl enable x11vnc.service
 systemctl start x11vnc.service
@@ -36,6 +41,7 @@ systemctl daemon-reload
 # Cleanup
 X11VNC_PASSWD=
 
-
+info "Script completed."
+exit 0
 
 
