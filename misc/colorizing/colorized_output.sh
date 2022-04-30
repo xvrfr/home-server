40e8c2b345a3333ed5ec5eb2928d47795ba03f68
xvrfr <xvr@bk.ru>
2022-04-30 15:17:53 +0300

    Update colorized_output.sh

--- misc/colorizing/colorized_output.sh
+++ misc/colorizing/colorized_output.sh
@@ -5,7 +5,14 @@ echo "\$#=$#"
 echo "\$1=$1"
 echo "\$0=$0"
 
+declare -A __colorized_output_en=(
+["info"="Info"]
+["warn"="Warning"]
+["error"="Error"]
+)
+
 #function info { echo -e "\e[32m[info] $*\e[39m"; }
 #function warn  { echo -e "\e[33m[warn] $*\e[39m"; }
 #function error { echo -e "\e[31m[error] $*\e[39m"; exit 1; }
 
+for sound in "${!__colorized_output_en[@]}"; do echo "$sound - ${__colorized_output_en[$sound]}"; done
