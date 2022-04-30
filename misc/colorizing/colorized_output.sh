e942dc353f996ff5e9f63ec992a5023bcaf06720
xvrfr <xvr@bk.ru>
2022-04-30 12:53:12 +0300

    Create colorized_output.sh

---
+++ misc/colorizing/colorized_output.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+echo "\$#=$#"
+echo "\$1=$1"
+
+#function info { echo -e "\e[32m[info] $*\e[39m"; }
+#function warn  { echo -e "\e[33m[warn] $*\e[39m"; }
+#function error { echo -e "\e[31m[error] $*\e[39m"; exit 1; }
+
