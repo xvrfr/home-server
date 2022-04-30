d00d1415af96a392f9d2396579fb3a6915c8fcca
xvrfr <xvr@bk.ru>
2022-04-30 19:13:13 +0300

    Update colorized_output.sh

--- misc/colorizing/colorized_output.sh
+++ misc/colorizing/colorized_output.sh
@@ -17,7 +17,9 @@ __colorized_output_lng="en"
 declare -n __localization_dict=__colorized_output_str_$1
 echo 3
 if [[ "${#__localization_dict[@]" == "0" ]] ; then
+  echo 31
   __localization_dict=__colorized_output_str_en
+  echo 32
 fi
 echo 4
 echo "Dict_length=${#__localization_dict[@]}"
