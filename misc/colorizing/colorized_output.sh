1405e1eb6ff939439fc1e3a7d79b22771a3005d3
xvrfr <xvr@bk.ru>
2022-04-30 19:04:43 +0300

    Update colorized_output.sh

--- misc/colorizing/colorized_output.sh
+++ misc/colorizing/colorized_output.sh
@@ -5,11 +5,26 @@ echo "\$#=$#"
 echo "\$1=$1"
 echo "\$0=$0"
 
+
 declare -A __colorized_output_str_en=(
-["info"]="Info"
-["warn"]="Warning"
-["error"]="Error"
+  ["info"]="Info"
+  ["warn"]="Warning"
+  ["error"]="Error"
 )
+
+__colorized_output_lng="en"
+
+declare -n __localization_dict=__colorized_output_str_$1
+
+if [[ "${#__localization_dict[@]" == "0" ]] ; then
+  __localization_dict=__colorized_output_str_en
+fi
+
+echo "Dict_length=${#__localization_dict[@]}"
+
+
+#echo "${#${!'__colorized_output_str_'__colorized_output_lng}}"
+
 
 #function info { echo -e "\e[32m[info] $*\e[39m"; }
 #function warn  { echo -e "\e[33m[warn] $*\e[39m"; }
