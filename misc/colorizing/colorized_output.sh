6ef2cca22a0107427688791ac5ceec0b861de432
xvrfr <xvr@bk.ru>
2022-04-30 19:22:56 +0300

    Update colorized_output.sh

--- misc/colorizing/colorized_output.sh
+++ misc/colorizing/colorized_output.sh
@@ -11,19 +11,19 @@ declare -A __colorized_output_str_en=(
   ["warn"]="Warning"
   ["error"]="Error"
 )
-echo 1
+
 __colorized_output_lng="en"
 
 declare -n __localization_dict=__colorized_output_str_$1
-echo 3
+
 if [[ "${#__localization_dict[@]}" == "0" ]] ; then
   echo 31
   __localization_dict=__colorized_output_str_en
   echo 32
 fi
-echo 4
+
 echo "Dict_length=${#__localization_dict[@]}"
-echo 5
+
 
 #echo "${#${!'__colorized_output_str_'__colorized_output_lng}}"
 
@@ -32,4 +32,4 @@ echo 5
 #function warn  { echo -e "\e[33m[warn] $*\e[39m"; }
 #function error { echo -e "\e[31m[error] $*\e[39m"; exit 1; }
 
-for sound in "${!__colorized_output_en[@]}"; do echo "$sound - ${__colorized_output_en[$sound]}"; done
+for sound in "${!__localization_dict[@]}"; do echo "$sound - ${__localization_dict[$sound]}"; done
