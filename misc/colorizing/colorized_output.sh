b3cd78d457b78e6ec69410c0e2803a10b75c051c
xvrfr <xvr@bk.ru>
2022-05-05 22:02:50 +0300

    Update colorized_output.sh

--- misc/colorizing/colorized_output.sh
+++ misc/colorizing/colorized_output.sh
@@ -2,49 +2,14 @@
 set -e
 
 __script_baseurl="https://github.com/xvrfr/home-server/raw/main/misc/colorizing/"
-__script_basename="colorized_output"
+__script_basename="colorized_messaging"
 __script_baselang="en"
 
-curl -sL  -H 'Cache-Control: no-cache' "https://github.com/xvrfr/home-server/raw/main/misc/translate.script_template" | \
-envsubst | cat
+source <(curl -sL "https://github.com/xvrfr/home-server/raw/main/misc/translate.script_template")
+__localize_dict_$__script_basename ru
+#__colorized_output_localize info ru
 
-echo 992
-
-
-echo "\$#=$#"
-echo "\$1=$1"
-echo "\$0=$0"
-
-#source <(curl -sL "https://github.com/xvrfr/home-server/raw/main/misc/colorizing/showcolors16.sh")
-
-function __colorized_output_localize {
-
-  source <(curl -sL "$__script_baseurl$__script_basename")
-
-
-  __colorized_output_lng="en"
-
-  declare -n __localization_dict=__colorized_output_str_${2,,}
-
-  if [[ "${#__localization_dict[@]}" == "0" ]] ; then
-    declare -n __localization_dict=__colorized_output_str_${__colorized_output_lng,,}
-  fi
-
-  __localization_key="${1,,}"
-
-  if [[ "${__localization_dict[$__localization_key]}" == "" ]] ; then
-    echo "$1"
-  else echo "${__localization_dict[$__localization_key]}"
-  fi
-
-}
-
-#echo "Dict_length=${#__localization_dict[@]}"
-#echo "${#${!'__colorized_output_str_'__colorized_output_lng}}"
-
-__colorized_output_localize info ru
-
-function info { echo -e "\e[32m[$(__colorized_output_localize info ru)] $*\e[39m"; }
+function info { echo -e "\e[32m[$(__localize_$__script_basename info ru)] $*\e[39m"; }
 #function warn  { echo -e "\e[33m[warn] $*\e[39m"; }
 #function error { echo -e "\e[31m[error] $*\e[39m"; exit 1; }
 
