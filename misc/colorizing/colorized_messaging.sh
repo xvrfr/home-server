4be22f1b8cfe87443a9c7cee6326b6c17f5a254a
xvrfr <xvr@bk.ru>
2022-05-05 22:16:15 +0300

    Update colorized_messaging.sh

--- misc/colorizing/colorized_messaging.sh
+++ misc/colorizing/colorized_messaging.sh
@@ -6,14 +6,14 @@ __script_basename="colorized_messaging"
 __script_baselang="en"
 
 source <(curl -sL "https://github.com/xvrfr/home-server/raw/main/misc/translate.script_template")
-__localize_dict_$__script_basename ru
+$(__localize_dict_$__script_basename ru)
 #__colorized_output_localize info ru
 
-function info { echo -e "\e[32m[$(__localize_$__script_basename info ru)] $*\e[39m"; }
+function info { echo -e "\e[32m[$(__localize_$__script_basename info)] $*\e[39m"; }
 #function warn  { echo -e "\e[33m[warn] $*\e[39m"; }
 #function error { echo -e "\e[31m[error] $*\e[39m"; exit 1; }
 
 #for sound in "${!__localization_dict[@]}"; do echo "$sound - ${__localization_dict[$sound]}"; done
 
-info "989"
+info "988"
 
